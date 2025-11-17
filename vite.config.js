0c7d4a6d3b4ff4d6f7ae31cd77f60b8dacfee7f0
Manybugs <me@manybugs.dev>
2025-11-17 22:18:41 +0900

    feat: remove all files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import elmPlugin from "vite-plugin-elm";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  plugins: [elmPlugin(), tailwindcss()],
-});
